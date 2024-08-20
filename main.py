f770ae59f1304c65e8ab927f95253d9959c798a7
sekvoja <sekvoja@sekvoja>
2024-08-20 16:12:13 +0300

    Вынес стили

--- main.py
+++ main.py
@@ -16,47 +16,8 @@ from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushBut
 from PyQt5.QtGui import QPixmap, QPalette, QBrush, QIcon
 from PyQt5.QtCore import Qt, pyqtSignal, QThread, QTimer, QSize
 
-# Встроенное содержимое style.qss
-style_qss = """
-QLabel#title {
-    color: white;
-    font-size: 24px;
-    font-weight: bold;
-    text-align: center;
-}
+from style import style_qss
 
-QPushButton {
-    background-color: #444;
-    color: white;
-    border-radius: 5px;
-    padding: 10px;
-}
-
-QLabel {
-    color: white;
-}
-
-QPushButton:hover {
-    background-color: #666;
-}
-
-QProgressBar {
-    background-color: #ccc;
-    border-radius: 5px;
-    text-align: center;
-}
-
-QProgressBar::chunk {
-    background-color: #0057e7;
-    width: 20px;
-}
-QToolButton {
-    background-color: black;
-    border: none;
-    background: transparent;
-    padding: 0px;
-}
-"""
 
 # Инициализация API Google Drive
 SCOPES = ['https://www.googleapis.com/auth/drive']
---
+++ style.py
@@ -0,0 +1,40 @@
+style_qss = """
+QLabel#title {
+    color: white;
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+}
+
+QPushButton {
+    background-color: #444;
+    color: white;
+    border-radius: 5px;
+    padding: 10px;
+}
+
+QLabel {
+    color: white;
+}
+
+QPushButton:hover {
+    background-color: #666;
+}
+
+QProgressBar {
+    background-color: #ccc;
+    border-radius: 5px;
+    text-align: center;
+}
+
+QProgressBar::chunk {
+    background-color: #0057e7;
+    width: 20px;
+}
+QToolButton {
+    background-color: black;
+    border: none;
+    background: transparent;
+    padding: 0px;
+}
+"""
